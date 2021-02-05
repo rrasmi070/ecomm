2f567332de512c9e468b884c9c91d6c703517309
rrasmi070 <rrasmi070@gmail.com>
2021-02-05 19:37:43 +0530

    host allow

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '-oy*p@ok6+^fl$%dd=1q^^i5lc3c=+!*cietgsl=!n$-1#w+ts'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','youned.herokuapp.com/']
 
 
 # Application definition
